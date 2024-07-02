1393947917fa138c6b9aa7a0dbd361877ef218a5
erickandreww <erick.andrew.moura@gmail.com>
2024-07-01 23:39:21 -0300

    "Learning 1 - week 4"

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const errorRoute = require("./routes/errorRoute")
 const utilities = require("./utilities/")
-// const pool = require("./database")
+const session = require("express-session")
+const pool = require('./database/')
 
 /* ***********************
 * View Engine and Templates
@@ -23,6 +24,26 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") //not at views root
 
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
