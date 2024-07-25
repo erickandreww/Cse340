3ee0a67aa5d38c75442b57d6d374247c361e8fb0
erickandreww <erick.andrew.moura@gmail.com>
2024-07-24 21:14:20 -0300

    "Final Project - Final Part"

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -3,7 +3,7 @@ const express = require("express")
 const utilities = require("../utilities/")
 const router = new express.Router()
 const reviewController = require("../controllers/reviewController")
-// const reviewValidate = require('../utilities/review-validation')
+const reviewValidate = require('../utilities/review-validation')
 
 router.post("/addReview", utilities.handleErrors(reviewController.addReview))
 
@@ -11,12 +11,12 @@ router.post("/addReview", utilities.handleErrors(reviewController.addReview))
 router.get("/editReview/:review_id", utilities.handleErrors(reviewController.buildEditReview))
 router.post(
     "/updateReview", 
-    // invValidate.newVehicleRules(),
-    // invValidate.checkUpdateData,
+    reviewValidate.reviewRules(),
+    reviewValidate.checkReviewEdit,
     utilities.handleErrors(reviewController.updateReview)
 );
 
-router.get("/deleteReview/:review_id", utilities.checkAccountType, utilities.handleErrors(reviewController.buildDeleteReview))
+router.get("/deleteReview/:review_id", utilities.handleErrors(reviewController.buildDeleteReview))
 router.post("/eraseReview", utilities.handleErrors(reviewController.deleteReview))
 
 
