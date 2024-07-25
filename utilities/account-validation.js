4521f4514e9da92a0ed04254fce5e4bb68adf911
erickandreww <erick.andrew.moura@gmail.com>
2024-07-24 22:23:04 -0300

    "account validate adjusts"

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -27,15 +27,11 @@ validate.registationRules = () => {
       // valid email is required and cannot already exist in the DB
       body("account_email")
         .trim()
+        .escape()
+        .notEmpty
         .isEmail()
         .normalizeEmail() // refer to validator.js docs
-        .withMessage("A valid email is required.")
-        .custom(async (account_email) => {
-          const emailExists = await accountModel.checkExistingEmail(account_email)
-          if (emailExists){
-            throw new Error("Email exists. Please log in or use different email")
-          }
-        }),
+        .withMessage("A valid email is required."),
   
       // password is required and must be strong password
       body("account_password")
